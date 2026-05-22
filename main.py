53b5092fd1315f61d42f7f94c663be760d6b8c7f
Dion Finnerty <103545669@student.swin.edu.au>
2026-05-22 16:11:58 +1000

    practical implementation of antispoof

--- main.py
+++ main.py
@@ -1,28 +1,50 @@
 import numpy as np
 import cv2 as cv
+import tensorflow as tf
+
+from model.antispoof_preprocess import predict_liveness
+
+antispood_raw = tf.keras.models.load_model("./model/models/antispoof_raw.keras")
+antispoof_transfer = tf.keras.models.load_model("./model/models/antispoof_transfer.keras")
+
+if not antispood_raw:
+    ValueError("model not found")
+if not antispoof_transfer:
+    ValueError("model not found")
 
 
 def main():
-    cap = cv.VideoCapture(0)
+    cap = cv.VideoCapture(1)
     if not cap.isOpened():
         print("Cannot open camera")
         exit()
     while True:
-        # Capture frame-by-frame
+    
         ret, frame = cap.read()
     
-        # if frame is read correctly ret is True
         if not ret:
             print("Can't receive frame (stream end?). Exiting ...")
             break
-        # Our operations on the frame come here
+        
+        label, score = predict_liveness(antispoof_transfer, frame, threshold=0.7)
+
+        cv.putText(
+            frame,
+            f"{label}: {score:.2f}",
+            (30, 50),
+            cv.FONT_HERSHEY_SIMPLEX,
+            1,
+            (0, 255, 0),
+            2
+        )
+        
         gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        # Display the resulting frame
+        
         cv.imshow('frame', gray)
         if cv.waitKey(1) == ord('q'):
             break
     
-    # When everything done, release the capture
+    
     cap.release()
     cv.destroyAllWindows()
 
