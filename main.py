b1eb38d02c4a8a09b43c25e471a854173a373cd2
Sovithyea Prach <154717400+sovithyea@users.noreply.github.com>
2026-05-23 22:00:07 +1000

    add antispoof model (v3) with balanced dataset, added RGB to preprocessing, added v3 to main.py

--- main.py
+++ main.py
@@ -4,47 +4,33 @@ import tensorflow as tf
 
 from model.antispoof_preprocess import predict_liveness
 
-antispood_raw = tf.keras.models.load_model("./model/models/antispoof_raw.keras")
+antispoof_raw = tf.keras.models.load_model("./model/models/antispoof_raw.keras")
 antispoof_transfer = tf.keras.models.load_model("./model/models/antispoof_transfer.keras")
-
-if not antispood_raw:
-    ValueError("model not found")
-if not antispoof_transfer:
-    ValueError("model not found")
+antispoof_v3 = tf.keras.models.load_model("./model/models/antispoof_v3.keras")
 
 
 def main():
-    cap = cv.VideoCapture(1)
+    cap = cv.VideoCapture(0) # 0 works for my camera, but you may need to change it to 1 or 2 if you have multiple cameras
     if not cap.isOpened():
         print("Cannot open camera")
         exit()
-    while True:
-    
-        ret, frame = cap.read()
-    
-        if not ret:
-            print("Can't receive frame (stream end?). Exiting ...")
-            break
-        
-        label, score = predict_liveness(antispoof_transfer, frame, threshold=0.7)
 
-        cv.putText(
-            frame,
-            f"{label}: {score:.2f}",
-            (30, 50),
-            cv.FONT_HERSHEY_SIMPLEX,
-            1,
-            (0, 255, 0),
-            2
-        )
+    while True:
+        ret, frame = cap.read()
+        if not ret:
+            print("Can't receive frame. Exiting...")
+            break
+
+        label, score = predict_liveness(antispoof_v3, frame, threshold=0.55)
+
+        color = (0, 255, 0) if label == "Real" else (0, 0, 255)
+        cv.putText(frame, f"{label}: {score:.2f}", (30, 50),
+                   cv.FONT_HERSHEY_SIMPLEX, 1, color, 2)
         
-        gray = cv.cvtColor(frame, cv.COLOR_BGR2GRAY)
-        
-        cv.imshow('frame', gray)
+        cv.imshow('frame', frame)
         if cv.waitKey(1) == ord('q'):
             break
-    
-    
+
     cap.release()
     cv.destroyAllWindows()
 
--- model/antispoof_preprocess.py
+++ model/antispoof_preprocess.py
@@ -1,9 +1,10 @@
 import numpy as np
-import cv2 as cv 
+import cv2 as cv
 import tensorflow as tf
 
 
 def predict_liveness(model, frame, threshold=0.7):
+    frame = cv.cvtColor(frame, cv.COLOR_BGR2RGB)
     input_frame = cv.resize(frame, (224, 224))
     input_frame = np.expand_dims(input_frame, axis=0)
 
@@ -13,5 +14,3 @@ def predict_liveness(model, frame, threshold=0.7):
         return "Spoof", pred
     else:
         return "Real", pred
-    
-    
